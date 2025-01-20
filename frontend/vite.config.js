e8edf53fcae22c174a0b594b3a7dcb98ce5f9d9d
sakthi <sakthi12qaz@gmail.com>
2025-01-20 23:38:18 +0530

    proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,23 +1,16 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy: {
-      // HTTP API proxy (for normal API requests)
-      "/api": {
-        target: "http://54.196.229.75:8000", // Backend API URL
-        // changeOrigin: true, // Optional: handles CORS issues by changing the Origin header
-        // rewrite: (path) => path.replace(/^\/api/, ""), // Optionally rewrite paths if needed
-      },
-      // WebSocket proxy
-      "/ws": {
-        target: "ws://54.196.229.75:8000/", // WebSocket server URL
-        ws: true, // This tells Vite to handle WebSocket traffic
-        // changeOrigin: true, // Optional: fixes CORS issues with WebSocket
+      proxy: {
+        '/api': {
+          target: 'http://54.196.229.75:8000',
+          changeOrigin: true,
+          // rewrite: (path) => path.replace(/^\/api/, ''),
+        },
       },
     },
-  },
-  
 });
