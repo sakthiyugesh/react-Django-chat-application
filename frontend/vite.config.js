e33b0e4de9de0ee237fb06f35916df46be65c1b7
sakthi <sakthi12qaz@gmail.com>
2025-01-20 22:13:52 +0530

    cors

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
-import { defineConfig } from 'vite';
-
 export default defineConfig({
   server: {
     proxy: {
