9ba64cd6dc63d7769edafb016f99d4a9f7124b39
sakthi <sakthi12qaz@gmail.com>
2025-01-20 23:29:14 +0530

    commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,14 +8,14 @@ export default defineConfig({
       // HTTP API proxy (for normal API requests)
       "/api": {
         target: "http://54.196.229.75:8000", // Backend API URL
-        changeOrigin: true, // Optional: handles CORS issues by changing the Origin header
+        // changeOrigin: true, // Optional: handles CORS issues by changing the Origin header
         // rewrite: (path) => path.replace(/^\/api/, ""), // Optionally rewrite paths if needed
       },
       // WebSocket proxy
       "/ws": {
         target: "ws://54.196.229.75:8000/", // WebSocket server URL
         ws: true, // This tells Vite to handle WebSocket traffic
-        changeOrigin: true, // Optional: fixes CORS issues with WebSocket
+        // changeOrigin: true, // Optional: fixes CORS issues with WebSocket
       },
     },
   },
